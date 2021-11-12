8b3db8b91e35a4f989512f605f2a2246af5e7fe6
ethanl82 <ethanl82@BHSCUR149020SPW.kprdsb.schools.local>
2021-11-12 12:51:20 -0500

    imports

--- src/main/java/lowe/battleship/FXMLController.java
+++ src/main/java/lowe/battleship/FXMLController.java
@@ -11,16 +11,217 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class FXMLController implements Initializable {
     
     @FXML
     private Label lblOut;
-    
+
     @FXML
-    private void btnClickAction(ActionEvent event) {
-        lblOut.setText("Hello World!");
+    private Button btnA1;
+
+    @FXML
+    private Button btnA2;
+
+    @FXML
+    private Button btnA3;
+
+    @FXML
+    private Button btnA4;
+
+    @FXML
+    private Button btnA5;
+
+    @FXML
+    private Button btnB2;
+
+    @FXML
+    private Button btnC1;
+
+    @FXML
+    private Button btnD1;
+
+    @FXML
+    private Button btnE1;
+
+    @FXML
+    private Button btnB1;
+
+    @FXML
+    private Button btnB3;
+
+    @FXML
+    private Button btnB4;
+
+    @FXML
+    private Button btnB5;
+
+    @FXML
+    private Button btnC2;
+
+    @FXML
+    private Button btnC3;
+
+    @FXML
+    private Button btnC4;
+
+    @FXML
+    private Button btnC5;
+
+    @FXML
+    private Button btnD2;
+
+    @FXML
+    private Button btnD3;
+
+    @FXML
+    private Button btnD4;
+
+    @FXML
+    private Button btnD5;
+
+    @FXML
+    private Button btnE2;
+
+    @FXML
+    private Button btnE3;
+
+    @FXML
+    private Button btnE4;
+
+    @FXML
+    private Button btnE5;
+
+    
+     @FXML
+    void btnA1a(ActionEvent event) {
+
     }
+
+    @FXML
+    void btnA2a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnA3a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnA4a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnA5a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnB1a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnB2a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnB3a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnB4a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnB5a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnC1a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnC2a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnC3a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnC4a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnC5a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnD1a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnD2a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnD3a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnD4a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnD5a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnE1a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnE2a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnE3a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnE4a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnE5a(ActionEvent event) {
+
+    }
+    public void btnpress (){
+    
+}
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
