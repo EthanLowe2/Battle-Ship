a2f70488b0a07a31925fd15c0670c54125de44fb
ethanl82 <ethanl82@BHSCUR149020SPW.kprdsb.schools.local>
2021-11-16 14:27:00 -0500

    Clicking

--- src/main/java/lowe/battleship/FXMLController.java
+++ src/main/java/lowe/battleship/FXMLController.java
@@ -13,6 +13,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
@@ -23,7 +24,11 @@ public class FXMLController implements Initializable {
 
     @FXML
     void MClick(MouseEvent event) {
-        Button btn = (Button) event.getSource();
+        ImageView img = (ImageView) event.getSource();
+        if (img.getAccessibleText().equals("X")){
+            img.setImage(new Image(getClass().getResource("/Wave Kaboom.jpg").toString()));
+        }else
+            
     }
     
     ImageView box[];
@@ -41,44 +46,71 @@ public class FXMLController implements Initializable {
 
     public void Ships() {
         if (Spot1 == 1 || Spot2 == 1) {
-            box[1].setAccessibleText("X");
-            box[6].setAccessibleText("X");
+            box[0].setAccessibleText("X");
+            box[5].setAccessibleText("X");
         } else if (Spot1 == 2 || Spot2 == 2) {
+            box[1].setAccessibleText("X");
             box[2].setAccessibleText("X");
-            box[3].setAccessibleText("X");
         } else if (Spot1 == 3 || Spot2 == 3) {
+            box[6].setAccessibleText("X");
             box[7].setAccessibleText("X");
-            box[8].setAccessibleText("X");
         } else if (Spot1 == 4 || Spot2 == 4) {
+            box[3].setAccessibleText("X");
+            box[8].setAccessibleText("X");
+        } else if (Spot1 == 5 || Spot2 == 5) {
             box[4].setAccessibleText("X");
             box[9].setAccessibleText("X");
-        } else if (Spot1 == 5 || Spot2 == 5) {
-            box[5].setAccessibleText("X");
-            box[10].setAccessibleText("X");
         } else if (Spot1 == 6 || Spot2 == 6) {
+            box[10].setAccessibleText("X");
             box[11].setAccessibleText("X");
-            box[12].setAccessibleText("X");
         } else if (Spot1 == 7 || Spot2 == 7) {
-            box[13].setAccessibleText("X");
-            box[18].setAccessibleText("X");
+            box[12].setAccessibleText("X");
+            box[17].setAccessibleText("X");
         } else if (Spot1 == 8 || Spot2 == 8) {
+            box[13].setAccessibleText("X");
             box[14].setAccessibleText("X");
-            box[15].setAccessibleText("X");
         } else if (Spot1 == 9 || Spot2 == 9) {
+            box[18].setAccessibleText("X");
             box[19].setAccessibleText("X");
-            box[20].setAccessibleText("X");
         } else if (Spot1 == 10 || Spot2 == 10) {
+            box[22].setAccessibleText("X");
             box[23].setAccessibleText("X");
-            box[24].setAccessibleText("X");
         } else if (Spot1 == 11 || Spot2 == 11) {
+            box[15].setAccessibleText("X");
+            box[20].setAccessibleText("X");
+        } else if (Spot1 == 12 || Spot2 == 12) {
             box[16].setAccessibleText("X");
             box[21].setAccessibleText("X");
-        } else if (Spot1 == 12 || Spot2 == 12) {
-            box[17].setAccessibleText("X");
-            box[22].setAccessibleText("X");
         }
     }
-
+    
+    public void Reset () {
+    box[0].setAccessibleText("");
+    box[1].setAccessibleText("");
+    box[2].setAccessibleText("");
+    box[3].setAccessibleText("");
+    box[4].setAccessibleText("");
+    box[5].setAccessibleText("");
+    box[6].setAccessibleText("");
+    box[7].setAccessibleText("");
+    box[8].setAccessibleText("");
+    box[9].setAccessibleText("");
+    box[10].setAccessibleText("");
+    box[11].setAccessibleText("");
+    box[12].setAccessibleText("");
+    box[13].setAccessibleText("");
+    box[14].setAccessibleText("");
+    box[15].setAccessibleText("");
+    box[16].setAccessibleText("");
+    box[17].setAccessibleText("");
+    box[18].setAccessibleText("");
+    box[19].setAccessibleText("");
+    box[20].setAccessibleText("");
+    box[21].setAccessibleText("");
+    box[22].setAccessibleText("");
+    box[23].setAccessibleText("");
+    box[24].setAccessibleText("");
+}
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         ImageView temp[] = {imgA1, imgA2, imgA3, imgA4, imgA5, imgB1, imgB2, imgB3, imgB4, imgB5, imgC1, imgC2, imgC3, imgC4, imgC5, imgD1, imgD2, imgD3, imgD4, imgD5, imgE1, imgE2, imgE3, imgE4, imgE5};
