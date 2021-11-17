22dac3789d168807f72ecf923d90614eabace912
ethanl82 <ethanl82@BHSCUR149020SPW.kprdsb.schools.local>
2021-11-17 13:40:55 -0500

    Real Working Game

--- src/main/java/lowe/battleship/FXMLController.java
+++ src/main/java/lowe/battleship/FXMLController.java
@@ -12,7 +12,6 @@ import java.util.concurrent.ThreadLocalRandom;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -28,7 +27,7 @@ public class FXMLController implements Initializable {
         if (img.getAccessibleText().equals("X")){
             img.setImage(new Image(getClass().getResource("/Wave Kaboom.jpg").toString()));
         }else
-            
+            img.setImage(new Image(getClass().getResource("/Wave Sploosh.jpg").toString()));
     }
     
     ImageView box[];
@@ -48,28 +47,28 @@ public class FXMLController implements Initializable {
         if (Spot1 == 1 || Spot2 == 1) {
             box[0].setAccessibleText("X");
             box[5].setAccessibleText("X");
-        } else if (Spot1 == 2 || Spot2 == 2) {
+        } if (Spot1 == 2 || Spot2 == 2) {
             box[1].setAccessibleText("X");
             box[2].setAccessibleText("X");
-        } else if (Spot1 == 3 || Spot2 == 3) {
+        } if (Spot1 == 3 || Spot2 == 3) {
             box[6].setAccessibleText("X");
             box[7].setAccessibleText("X");
-        } else if (Spot1 == 4 || Spot2 == 4) {
+        } if (Spot1 == 4 || Spot2 == 4) {
             box[3].setAccessibleText("X");
             box[8].setAccessibleText("X");
-        } else if (Spot1 == 5 || Spot2 == 5) {
+        } if (Spot1 == 5 || Spot2 == 5) {
             box[4].setAccessibleText("X");
             box[9].setAccessibleText("X");
-        } else if (Spot1 == 6 || Spot2 == 6) {
+        } if (Spot1 == 6 || Spot2 == 6) {
             box[10].setAccessibleText("X");
             box[11].setAccessibleText("X");
-        } else if (Spot1 == 7 || Spot2 == 7) {
+        } if (Spot1 == 7 || Spot2 == 7) {
             box[12].setAccessibleText("X");
             box[17].setAccessibleText("X");
-        } else if (Spot1 == 8 || Spot2 == 8) {
+        } if (Spot1 == 8 || Spot2 == 8) {
             box[13].setAccessibleText("X");
             box[14].setAccessibleText("X");
-        } else if (Spot1 == 9 || Spot2 == 9) {
+        } if (Spot1 == 9 || Spot2 == 9) {
             box[18].setAccessibleText("X");
             box[19].setAccessibleText("X");
         } else if (Spot1 == 10 || Spot2 == 10) {
@@ -85,32 +84,18 @@ public class FXMLController implements Initializable {
     }
     
     public void Reset () {
-    box[0].setAccessibleText("");
-    box[1].setAccessibleText("");
-    box[2].setAccessibleText("");
-    box[3].setAccessibleText("");
-    box[4].setAccessibleText("");
-    box[5].setAccessibleText("");
-    box[6].setAccessibleText("");
-    box[7].setAccessibleText("");
-    box[8].setAccessibleText("");
-    box[9].setAccessibleText("");
-    box[10].setAccessibleText("");
-    box[11].setAccessibleText("");
-    box[12].setAccessibleText("");
-    box[13].setAccessibleText("");
-    box[14].setAccessibleText("");
-    box[15].setAccessibleText("");
-    box[16].setAccessibleText("");
-    box[17].setAccessibleText("");
-    box[18].setAccessibleText("");
-    box[19].setAccessibleText("");
-    box[20].setAccessibleText("");
-    box[21].setAccessibleText("");
-    box[22].setAccessibleText("");
-    box[23].setAccessibleText("");
-    box[24].setAccessibleText("");
+        for (ImageView name : box) {  
+          name.setAccessibleText("");
+          name.setImage(new Image(getClass().getResource("/Wave.jpg").toString()));
+   }   
 }
+    @FXML
+    void btnPlay(ActionEvent event) {
+        Reset();
+        Slots();
+        Ships();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         ImageView temp[] = {imgA1, imgA2, imgA3, imgA4, imgA5, imgB1, imgB2, imgB3, imgB4, imgB5, imgC1, imgC2, imgC3, imgC4, imgC5, imgD1, imgD2, imgD3, imgD4, imgD5, imgE1, imgE2, imgE3, imgE4, imgE5};
