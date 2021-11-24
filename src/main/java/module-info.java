d7545d6290710575224c4827fa79240d21c46ac0
ethanl82 <ethanl82@BHSCUR149020SPW.kprdsb.schools.local>
2021-11-24 14:13:04 -0500

    Added a start screem

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module Lowe {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires java.base;
     opens lowe.battleship to javafx.fxml;
     exports lowe.battleship;
 }
